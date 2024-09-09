cb5a3a0779e74f4f163a7a3dd3ddf278198341c7
lsullo <lsullo2004@gmail.com>
2024-09-09 11:47:10 -0500

    All take 2

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -44,8 +44,7 @@ const schema = a.schema({
             groupname: a.string().required(),
             groupUrlName: a.string().required(),
             gmessages: a.hasMany('GroupMessage', 'groupId'),
-            adminId: a.id().required(),
-            groupuser: a.hasMany('User', 'groupId'),
+            adminId: a.id().required(),  // The user who created the group chat
             //members: a.hasMany('GroupChatUser', 'groupId'),
             //isPrivate: a.boolean().default(false),
             
@@ -92,7 +91,7 @@ const schema = a.schema({
 export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
-    name: 'luke-chat-test1',
+    name: 'luke-chat',
     schema,
     authorizationModes: {
         defaultAuthorizationMode: 'userPool',
