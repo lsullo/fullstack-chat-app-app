440c016f80f9cf13760127189b97dacdfb7bcc61
lsullo <lsullo2004@gmail.com>
2024-11-08 19:03:31 -0600

    test of the lambda function "myLambdaFunction"🍞🔜

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,13 +1,15 @@
-import { storage } from './storage/resource'
-import { defineBackend } from '@aws-amplify/backend'
-import { auth } from './auth/resource'
-import { data } from './data/resource'
+import { storage } from './storage/resource';
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
+import { myLambdaFunction } from './functions/myLambdaFunction/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-	auth,
-	data,
-	storage,
-})
+export const backend = defineBackend({
+  auth,
+  data,
+  storage,
+  myLambdaFunction, 
+});
