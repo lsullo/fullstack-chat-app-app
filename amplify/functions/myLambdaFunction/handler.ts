9407e653b1e1360c080fa8c4dec8490d15fc9e41
lsullo <lsullo2004@gmail.com>
2024-11-16 17:29:49 -0600

    test of function changes...

--- amplify/functions/myLambdaFunction/handler.ts
+++ amplify/functions/myLambdaFunction/handler.ts
@@ -1,18 +1,30 @@
-import type { Handler } from 'aws-lambda';
-import { Schema } from '../../data/resource';
+import { Handler } from 'aws-lambda';
+import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
+import outputs from '../../../amplify_outputs.json'; // Ensure this points to your actual Amplify configuration file
+import { Schema } from '../../data/resource';
+
+Amplify.configure(outputs); // Configure Amplify with the correct configuration
 
 export const handler: Handler = async (event, context) => {
-  const groupId = '22084a9d-05f2-4752-8a11-b43c33736472'
-  const client = generateClient<Schema>();
+  try {
+    const groupId = '22084a9d-05f2-4752-8a11-b43c33736472';
+    const client = generateClient<Schema>();
 
-              if (groupId) {
-                await client.models.GroupMessage.create({
-                  groupId: groupId,
-                  type: 'system',
-                  content: `ACP ACTIVATED ANTI_GAY ON`,
-                  userNickname: 'LTM',
-                });
-              }
-  return 'Hello, World!';
+    if (groupId) {
+      await client.models.GroupMessage.create({
+        groupId: groupId,
+        type: 'system',
+        content: `ACP ACTIVATED ANTI_GAY ON`,
+        userNickname: 'LTM',
+      });
+    }
+
+    return {
+      statusCode: 200,
+      body: 'Hello, World!',
+    };
+  } catch (error) {
+    console.error('Error:', error);
+  }
 };
