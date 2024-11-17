c66e4fa930f0af4f8c6f1734d1d4994532e0c288
lsullo <lsullo2004@gmail.com>
2024-11-16 21:05:07 -0600

    type shit

--- amplify/functions/myLambdaFunction/handler.ts
+++ amplify/functions/myLambdaFunction/handler.ts
@@ -1,10 +1,10 @@
 import { Handler } from 'aws-lambda';
 import { Amplify } from 'aws-amplify';
 import { generateClient } from 'aws-amplify/api';
-import outputs from '../../../amplify_outputs.json'; // Ensure this points to your actual Amplify configuration file
+//import outputs from '../../../amplify_outputs.json'; // Ensure this points to your actual Amplify configuration file
 import { Schema } from '../../data/resource';
 
-Amplify.configure(outputs); 
+//Amplify.configure(outputs); 
 export const handler: Handler = async (event, context) => {
   try {
     const groupId = '22084a9d-05f2-4752-8a11-b43c33736472';
