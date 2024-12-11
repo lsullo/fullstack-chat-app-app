69e98551c8fe2c4db7bb64425443da9b8a852bba
lsullo <lsullo2004@gmail.com>
2024-12-10 18:19:35 -0600

    forgot to remove shit fromo backend broad shit

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,6 @@ import { storage } from "./storage/resource";
 import { defineBackend } from "@aws-amplify/backend";
 import { auth } from "./auth/resource";
 import { data } from "./data/resource";
-import { myLambdaFunction } from "./functions/myLambdaFunction/resource";
 import { fullCircleLambda2 } from "./functions/fullCircleLambda2/resource";
 import { VIPCreator } from "./functions/VIPCreator/resource";
 
@@ -10,7 +9,6 @@ export const backend = defineBackend({
   auth,
   data,
   storage,
-  myLambdaFunction,
   fullCircleLambda2,
   VIPCreator,
 });
